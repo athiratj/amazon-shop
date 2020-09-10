605cfccf40592fb3fc367dfe7de43ace24e38902
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-09-10 19:30:18 +0530

    added header component and styling

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+function Header() {
+  return (
+    <div className="Header">
+      <img
+        className="Header_logo"
+        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="amazon logo"
+      />
+      <div className="Header_search">
+        <input type="text" className="Header_searchInput" />
+        <SearchIcon className="Header_searchIcon" />
+      </div>
+      <div className="Header_nav">
+        <div className="Header_option">
+          <span className="Header_optionLineOne">Hello User</span>
+          <span className="Header_optionLineTwo">Sign In </span>
+        </div>
+        <div className="Header_option">
+          <span className="Header_optionLineOne">Return</span>
+          <span className="Header_optionLineTwo">Orders</span>
+        </div>
+        <div className="Header_option">
+          <span className="Header_optionLineOne">Your</span>
+          <span className="Header_optionLineTwo">Prime</span>
+        </div>
+        <div className="Header_optionBasket">
+          <ShoppingBasketIcon />
+          <span className="Header_basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBfRDfOFDZPIvv_7diL134bE1MEoNo1kc8",
+  authDomain: "shop08-9a751.firebaseapp.com",
+  databaseURL: "https://shop08-9a751.firebaseio.com",
+  projectId: "shop08-9a751",
+  storageBucket: "shop08-9a751.appspot.com",
+  messagingSenderId: "191356918177",
+  appId: "1:191356918177:web:79133f5b38b143178453e0",
+  measurementId: "G-Q710Z2YSDX",
+};
